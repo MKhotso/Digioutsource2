167e9453707bac946839c3101787b6a710d86e3a
Mkhot <Mkhot@SHUGGYRAY>
2024-04-11 15:11:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of lines:");
+        int H = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = H; i >= 1; i--)
+        {
+            Console.WriteLine();
+            for (int j = i; j >= 1; j--)
+            {
+                Console.Write('-');
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
